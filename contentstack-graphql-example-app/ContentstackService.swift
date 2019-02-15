0559f71a9c7d4e09d2609604fe83b205ee266d26
Uttam K Ukkoji <uttamukkoji@gmail.com>
2019-02-16 00:13:02 +0530

    Prod URL updated

--- contentstack-graphql-example-app/ContentstackService.swift
+++ contentstack-graphql-example-app/ContentstackService.swift
@@ -13,7 +13,7 @@ class ContentstackService {
     let graphQLClient: ApolloClient
     
     init(credentials: ContentstackCredentials = ContentstackCredentials.default) {
-        let url = URL(string: "https://\(credentials.domainHost)/stacks/\(credentials.apiKey)/explore?environment=\(credentials.environmentToken)&access_token=\(credentials.deliveryAPIAccessToken)")!
+        let url = URL(string: "https://\(credentials.domainHost)/stacks/\(credentials.apiKey)?environment=\(credentials.environmentToken)&access_token=\(credentials.deliveryAPIAccessToken)")!
         self.graphQLClient = ApolloClient(networkTransport: HTTPNetworkTransport(url: url))
     }
 }
