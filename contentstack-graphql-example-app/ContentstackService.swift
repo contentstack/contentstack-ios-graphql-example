32bd0d3ac07b977bb4253c45d550d93400923db3
Uttam Krishna Ukkoji <uttamukkoji@gmail.com>
2021-01-06 13:17:46 +0530

    URL path v1 removed

--- contentstack-graphql-example-app/ContentstackService.swift
+++ contentstack-graphql-example-app/ContentstackService.swift
@@ -13,7 +13,7 @@ class ContentstackService {
     let graphQLClient: ApolloClient
     
     init(credentials: ContentstackCredentials = ContentstackCredentials.default) {
-        let url = URL(string: "https://\(credentials.domainHost)/v1/stacks/\(credentials.apiKey)?environment=\(credentials.environmentToken)")!
+        let url = URL(string: "https://\(credentials.domainHost)/stacks/\(credentials.apiKey)?environment=\(credentials.environmentToken)")!
         let configuration = URLSessionConfiguration.default
         configuration.httpAdditionalHeaders = ["access_token": credentials.deliveryAPIAccessToken]
         self.graphQLClient = ApolloClient(networkTransport: HTTPNetworkTransport(url: url, client: URLSessionClient(sessionConfiguration: configuration)))
